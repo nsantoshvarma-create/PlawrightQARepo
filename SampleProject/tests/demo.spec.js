315ba5761e6b3fad6302ad343128cc29e65f662f
nsantoshvarma-create <nsantoshvarma@gmail.com>
2026-03-29 09:56:11 +0530

    Initial Draft

---
+++ SampleProject/tests/demo.spec.js
@@ -0,0 +1,6 @@
+const {test, expect} = require('@playwright/test')
+ 
+test("First PLaywright Testcase", function display(){
+    console.log("It is a First PLaywright Testcases based on Named Function!!!");
+   
+})
---
+++ SampleProject/tests/demo1.spec.js
@@ -0,0 +1,6 @@
+const {test, expect} = require('@playwright/test')
+ 
+ 
+test("Second PLaywright Testcase", function(){
+    console.log("It is a Second PLaywright Testcases based on Anonymous Function!!!");
+})
